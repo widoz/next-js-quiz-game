1db13426d4efbea71c005db17d846c39a78a5f08
guido <dev@guidoscialfa.com>
2024-12-03 22:53:26 +0100

    Fix Hydratation Mismatch
    Signed-off-by: guido <dev@guidoscialfa.com>

--- src/components/theme-toggler.tsx
+++ src/components/theme-toggler.tsx
@@ -1,9 +1,19 @@
 import { Moon, Sun } from 'lucide-react'
 import { Button } from './ui/button'
 import { useTheme } from 'next-themes'
+import { useEffect, useState } from 'react'
 
 export function ThemeToggler() {
+    const [mounted, setMounted] = useState(false)
     const { theme, setTheme } = useTheme()
+
+    useEffect(() => {
+        setMounted(true)
+    }, [])
+
+    if (!mounted) {
+        return null
+    }
 
     return (
         <Button
