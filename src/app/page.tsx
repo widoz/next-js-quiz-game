3f6bf38212dd6fc31afcf7245b74ec1809e5f98b
guido <dev@guidoscialfa.com>
2024-12-03 20:38:25 +0100

    Only show the Debugger component when explicitly requested
    Signed-off-by: guido <dev@guidoscialfa.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,11 +4,15 @@ import { Card } from '@/components/card'
 import { Debugger } from '@/components/debugger'
 import { QuizProvider } from '@/components/state-context'
 import { ThemeToggler } from '@/components/theme-toggler'
+import { useSearchParams } from 'next/navigation'
 
 export default function Page() {
+    const searchParams = useSearchParams()
+    const debug = searchParams.get('debug')
+
     return (
         <QuizProvider>
-            <Debugger />
+            {!!debug && <Debugger />}
             <ThemeToggler />
             <main className="min-h-screen w-full flex items-center justify-center p-4">
                 <Card
